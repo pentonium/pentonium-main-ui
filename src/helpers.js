f062841bbdced2797d279324d7b15c886374816a
bornbig <72680443+bornbig@users.noreply.github.com>
2020-12-28 12:59:00 +0530

    Initial commit

---
+++ src/helpers.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+export function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
+}
+
+export function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+export function setJsonCookie(cname, cvalue, exdays) {
+  setCookie(cname, JSON.stringify(cvalue), exdays);
+}
+
+export function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+export function getJsonCookie(cname) {
+  return JSON.parse(getCookie(cname));
+}
+
+export function checkCookie(cname) {
+  var user = getCookie(cname);
+  if (user != "") {
+    return true;
+  }
+
+  return false;
+}
+
+export const slugToText = slug => {
+  var words = slug.split("_");
+
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+    words[i] = word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
+  return words.join(" ");
+};
+
+export const getUrlVars = () => {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(
+    m,
+    key,
+    value
+  ) {
+    vars[key] = value;
+  });
+  return vars;
+};
---
+++ src/reducers/commonReducer.js
@@ -0,0 +1,14 @@
+
+
+
+const initialState = {
+  loading: false,
+  error: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
