9c94c9b7ffe4a74f69b506bc64e0b0469f941710
Bhagat Yedla <byedla@xpanxion.com>
2021-02-06 23:50:36 +0530

    Added breadcrumb

---
+++ src/controllers/SiteBreadCrumb.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { withRouter } from "react-router-dom";
+
+const Breadcrumbs = props => {
+  const {
+    history,
+    location: { pathname }
+  } = props;
+  const pathnames = pathname.split("/").filter(x => x);
+  return (
+    <Breadcrumb>
+    {pathnames.length > 0 ? (
+        <Breadcrumb.Item onClick={() => history.push("/")}>Home</Breadcrumb.Item>
+      ) : (
+        <Breadcrumb.Item active>Home</Breadcrumb.Item>
+      )}
+      {pathnames.map((name, index) => {
+        const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+        const isLast = index === pathnames.length - 1;
+        return isLast ? (
+            <Breadcrumb.Item active>{name}</Breadcrumb.Item>
+        ) : (
+          <Breadcrumb.Item key={name} onClick={() => history.push(routeTo)}>
+            {name} 
+          </Breadcrumb.Item>
+        );
+      })}
+    </Breadcrumb>
+  );
+};
+
+export default withRouter(Breadcrumbs);
