2b2bd05d1419fe5b7e2b1f7119fd9cf7518d3c0a
Bhagat Yedla <byedla@xpanxion.com>
2021-02-20 23:12:06 +0530

    Drop down menu

---
+++ src/components/DropDownMenuItem.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import {fetchCategories} from '../actions/categoryActions';
+
+
+
+class DropDownMenuItem extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount(){
+        const categoryId = this.props.id;
+        this.props.fetchCategories(categoryId);
+    }
+
+    render() { 
+        return (
+            <div className="row">
+                {this.props.categories}
+            </div>
+         );
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        categories: state.category.categoryItems
+      };
+  }
+  
+function mapDispatchToProps(dispatch){
+    return{
+        fetchCategories: (id) => dispatch(fetchCategories(id))
+    }
+}
+ 
+export default connect(mapStateToProps , mapDispatchToProps)(DropDownMenuItem);
